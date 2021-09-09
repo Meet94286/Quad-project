2f7f837fc13d0959d29acd01acc220ef46912a82
Meet94286 <mehtameet4321.mm@gmail.com>
2021-09-09 17:44:50 +0530

    code added

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Rates from "./Rates";
+import  Image  from "./Image";
+import Tables from "./Tables"
+import Image1  from "./Image1";
+import  "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Rates/>
+      <Image/>
+      <Tables/>
+      <Image1/>
+      <Footer/>
+      
+   
+    </div>
+  );
+};
+
+export default App;
+
